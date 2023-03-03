cbf7f0ff8a7cd732115888001819051cecd927cc
riccardodebenedictis <riccardo.debenedictis@istc.cnr.it>
2023-03-03 11:29:42 +0100

    minor cleanings..

--- include/lit.h
+++ include/lit.h
@@ -15,8 +15,21 @@ namespace semitone
   public:
     explicit constexpr lit(var v = std::numeric_limits<var>::max(), bool sign = true) : x((v << 1) + sign) {}
 
+    /**
+     * @brief Get the variable of the literal.
+     * 
+     * @param p The literal.
+     * @return var The variable of the literal.
+     */
     inline friend var variable(const lit &p) noexcept { return p.x >> 1; }
+    /**
+     * @brief Get the sign of the literal.
+     * 
+     * @param p The literal.
+     * @return bool The sign of the literal.
+     */
     inline friend bool sign(const lit &p) noexcept { return p.x & 1; }
+    
     inline friend size_t index(const lit &p) noexcept { return p.x; }
     inline friend bool is_undefined(const lit &p) noexcept { return p.x == std::numeric_limits<var>::max(); }
 
