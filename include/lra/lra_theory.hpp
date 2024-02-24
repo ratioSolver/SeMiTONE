c28140bf79846c667fe87e4d7204160adc395105
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-02-24 18:29:43 +0100

    Add new_var() function to lra_theory

--- include/lra/lra_theory.hpp
+++ include/lra/lra_theory.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <vector>
 #include "theory.hpp"
+#include "inf_rational.hpp"
+#include "tableau.hpp"
 
 namespace semitone
 {
@@ -15,5 +18,19 @@ namespace semitone
      * @return VARIABLE_TYPE the new variable.
      */
     VARIABLE_TYPE new_var() noexcept;
+
+  private:
+    /**
+     * Represents the bound of a variable and the reason for its existence.
+     */
+    struct bound
+    {
+      utils::inf_rational value; // the value of the bound..
+      lit reason;                // the reason for the value..
+    };
+
+    std::vector<bound> c_bounds;           // the current bounds..
+    std::vector<utils::inf_rational> vals; // the current values..
+    utils::tableau tableau;                // the tableau..
   };
 } // namespace semitone
--- src/lra/lra_theory.cpp
+++ src/lra/lra_theory.cpp
@@ -3,4 +3,13 @@
 namespace semitone
 {
     lra_theory::lra_theory(std::shared_ptr<sat_core> sat) : theory(sat) {}
+
+    VARIABLE_TYPE lra_theory::new_var() noexcept
+    {
+        auto var = vals.size();
+        c_bounds.emplace_back(bound{utils::inf_rational(utils::rational::negative_infinite), TRUE_lit});
+        c_bounds.emplace_back(bound{utils::inf_rational(utils::rational::positive_infinite), TRUE_lit});
+        vals.push_back(utils::inf_rational(utils::rational::zero));
+        return var;
+    }
 } // namespace semitone
