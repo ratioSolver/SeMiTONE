86e2e7821d4f5232fd29dafcf060a4167e5721d7
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-04-14 10:21:00 +0200

    Enable visualization feature in CMakeLists.txt and related files

--- include/clause.hpp
+++ include/clause.hpp
@@ -33,7 +33,9 @@ namespace semitone
 
     [[nodiscard]] std::vector<utils::lit> get_reason(const utils::lit &p) const noexcept override;
 
+#ifdef ENABLE_VISUALIZATION
     [[nodiscard]] json::json to_json() const noexcept override;
+#endif
 
   private:
     std::vector<utils::lit> lits;
